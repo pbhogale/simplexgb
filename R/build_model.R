63aa82eec996c532d5c4b350ccb0f2e783401394
Prasanna Bhogale <prasanna.bhogale@share-now.com>
2020-05-29 16:16:10 +0200

    updated predict method

--- R/build_model.R
+++ R/build_model.R
@@ -288,7 +288,7 @@ get_predictions_linear <- function(model_structure, test_df){
 
   if (model_structure[["models"]][['model_xgb']][["params"]][["objective"]] ==
       "multi:softprob") {
-    preds <- plogis(predict(model_structure[["models"]][['linear_model']],
+    preds <- plogis(stats::predict(model_structure[["models"]][['linear_model']],
                                            newx = features))
     prob_matrix <- matrix(preds, nrow = nrow(norm_test_df),
                           byrow = T)
@@ -311,7 +311,7 @@ get_predictions_linear <- function(model_structure, test_df){
     predictions[["category"]] <- cat_df[["category"]]
   }
   else {
-    preds <- predict(model_structure[["models"]][['linear_model']],
+    preds <- stats::predict(model_structure[["models"]][['linear_model']],
                                        newx = features)
     predictions <- tibble::tibble(prediction = preds[1:nrow(test_df)])
     colnames(predictions) <- model_structure[["target_variable"]]
